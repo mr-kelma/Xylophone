2d70aa12c14f14d3996fc2f9d95e7947826e2b64
Valery Keplin <98012564+mr-kelma@users.noreply.github.com>
2022-06-07 00:48:31 +0400

    Initial commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import AVFoundation
+
+var player: AVAudioPlayer!
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override var shouldAutorotate: Bool {
+        false
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        playSound(sender.titleLabel?.text ?? "errorSound")
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(800)) {
+            sender.alpha = 1
+        }
+    }
 }
 
+func playSound(_ resource: String) {
+    let url = Bundle.main.url(forResource: resource, withExtension: "mp3")
+    player = try! AVAudioPlayer(contentsOf: url!)
+    player.play()
+}
