66f8a74673fb7d64c6b2d562f990e9cf5e196d2a
Valery Keplin <98012564+mr-kelma@users.noreply.github.com>
2022-06-06 14:13:45 +0400

    Initial Commit

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Valery Keplin on 6.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
